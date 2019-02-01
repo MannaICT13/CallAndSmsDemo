e8ea153ce3a104780bff697944c5f8eb3dc091ab
Md Khaled Hasan Manna <khmannaict13@gmail.com>
2019-02-01 22:08:01 +0600

    Initial Commit

---
+++ CallAndSmsDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CallAndSmsDemo
+//
+//  Created by USER on 2/1/19.
+//  Copyright © 2019 mCubes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
